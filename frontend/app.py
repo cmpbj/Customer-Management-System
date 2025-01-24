c1b83a86ef88d04695d63ac2cadb503fa0325973
cmpbj <magnocarlos3232@gmail.com>
2025-01-23 21:21:55 -0300

    add frontend to the project

---
+++ frontend/app.py
@@ -0,0 +1,129 @@
+import streamlit as st
+import requests
+import pandas as pd
+
+st.set_page_config(layout="wide")
+
+# st.image("logo.png", width=200)
+
+st.title("Customer Management")
+
+
+def show_response_message(response):
+    if response.status_code == 200:
+        st.success("Operation carried out successfully!")
+    else:
+        try:
+            data = response.json()
+            if "detail" in data:
+                if isinstance(data["detail"], list):
+                    errors = "\n".join([error["msg"] for error in data["detail"]])
+                    st.error(f"Erro: {errors}")
+                else:
+                    st.error(f"Erro: {data['detail']}")
+        except ValueError:
+            st.error("Unknown error. Unable to decode the response.")
+
+
+with st.expander("Add a New Costumer"):
+    with st.form("new_Costumer"):
+        name = st.text_input("Costumer name")
+        email = st.text_input("Costumer e-mail")
+        phone = st.text_input("Phone")
+        address = st.text_area("Costumer address")
+        submit_button = st.form_submit_button("Add Customer")
+
+        if submit_button:
+            response = requests.post(
+                "http://backend:8000/customers/",
+                json={
+                    "name": name,
+                    "email": email,
+                    "phone": phone,
+                    "address": address,
+                },
+            )
+            show_response_message(response)
+
+with st.expander("View customers"):
+    if st.button("View All customers"):
+        response = requests.get("http://backend:8000/customers/")
+        if response.status_code == 200:
+            customer = response.json()
+            df = pd.DataFrame(customer)
+
+            if not df.empty:
+                df = df[
+                    [
+                        "id",
+                        "name",
+                        "email",
+                        "phone",
+                        "address",
+                        "created_at",
+                    ]
+                ]
+
+                st.write(df.to_html(index=False), unsafe_allow_html=True)
+            else:
+                st.info("No customers found.")
+        else:
+            show_response_message(response)
+
+with st.expander("Get Details of a customer"):
+    get_id = st.number_input("customer ID", min_value=1, format="%d")
+    if st.button("Search customer"):
+        response = requests.get(f"http://backend:8000/customers/{get_id}")
+        if response.status_code == 200:
+            customer = response.json()
+            df = pd.DataFrame([customer])
+
+            df = df[
+                [
+                    "id",
+                    "name",
+                    "email",
+                    "phone",
+                    "address",
+                    "created_at",
+                ]
+            ]
+
+            st.write(df.to_html(index=False), unsafe_allow_html=True)
+        else:
+            show_response_message(response)
+
+with st.expander("Delete customer"):
+    delete_id = st.number_input("customer ID to Delete", min_value=1, format="%d")
+    if st.button("Delete customer"):
+        response = requests.delete(f"http://backend:8000/customers/{delete_id}")
+        show_response_message(response)
+
+with st.expander("Update customer"):
+    with st.form("update_customer"):
+        update_id = st.number_input("Customer ID", min_value=1, format="%d")
+        new_name = st.text_input("New customer Name")
+        new_email = st.text_input("New customer e-mail")
+        new_phone = st.text_input("New phone")
+        new_address = st.text_area("New customer address")
+
+        update_button = st.form_submit_button("Update customer")
+
+        if update_button:
+            update_data = {}
+            if new_name:
+                update_data["name"] = new_name
+            if new_email:
+                update_data["email"] = new_email
+            if len(new_phone) > 0:
+                update_data["phone"] = new_phone
+            if new_address:
+                update_data["address"] = new_address
+
+            if update_data:
+                response = requests.put(
+                    f"http://backend:8000/customers/{update_id}", json=update_data
+                )
+                show_response_message(response)
+            else:
+                st.error("No information provided for update")
